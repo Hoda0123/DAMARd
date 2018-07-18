d45277229bd3c5f9731bf17b8373df6dc3c754a2
DAMAR2016 <40209506+DAMAR2016@users.noreply.github.com>
2018-07-18 14:49:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,9 @@ client.on('message', message => {
     if (message.content === 'ping') {
         message.reply('pong');
       }
+});
+
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require("fs");
